6f7df43ae899650c1353b24125fef632ec4772d3
Johan Ouwerkerk <jm.ouwerkerk@gmail.com>
2016-05-07 15:34:35 +0200

    Add test for checking computed HOTP tokens against sample test vector from Appendix D of RFC 4226 (the HOTP spec) See: https://www.ietf.org/rfc/rfc4226.txt

---
+++ autotests/otp/rfc4226-hotp-compat.cpp
@@ -0,0 +1,87 @@
+
+#include "otp/base32.h"
+#include "otp/token.h"
+
+#include <QTest>
+#include <QtDebug>
+
+class RFC4226TestVectorCompatTest: public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void initTestCase(void);
+    void testSample(void);
+    void testSample_data(void);
+private:
+    otp::Key m_key;
+    otp::Algorithm m_algo;
+    otp::Encoder m_encoder;
+};
+
+void RFC4226TestVectorCompatTest::initTestCase(void)
+{
+    m_key = otp::keyForAuthenticator();
+    m_algo = otp::hmacAlgorithm();
+    m_encoder = otp::otpEncoder();
+}
+
+// static void result(quint64 counter, const char * expected, int l)
+// {
+//
+//     qDebug() << "secret is:" << secret;
+//     QTest::newRow(qPrintable(QStringLiteral("RFC 4226 test vector, counter value = %1").arg(counter))) << secret << counter << QString::fromLocal8Bit(output);
+// }
+
+void RFC4226TestVectorCompatTest::testSample(void)
+{
+
+    QFETCH(QString, secret);
+    QFETCH(quint64, counter);
+    const otp::Message msg([counter](void) -> QByteArray
+    {
+        return otp::hotpTokenMessage(counter);
+    });
+
+
+    QTEST(otp::token(otp::base32::recode(secret), msg, m_key, m_algo, m_encoder), "rfc-test-vector");
+}
+
+static void result(int k, const QString& secret, const char * expected)
+{
+
+    QByteArray output(expected, 6);
+
+    QTest::newRow(qPrintable(QStringLiteral("RFC 4226 test vector, counter value = %1").arg(k))) << secret << (quint64) k << QString::fromLocal8Bit(output);
+}
+
+
+void RFC4226TestVectorCompatTest::testSample_data(void)
+{
+    static const char * corpus[10] {
+        "755224",
+        "287082",
+        "359152",
+        "969429",
+        "338314",
+        "254676",
+        "287922",
+        "162583",
+        "399871",
+        "520489"
+    };
+    static QLatin1String secret("12345678901234567890");
+
+    QTest::addColumn<QString>("secret");
+    QTest::addColumn<quint64>("counter");
+    QTest::addColumn<QString>("rfc-test-vector");
+
+    for(int k = 0; k < 10; ++k)
+    {
+        result(k, secret, corpus[k]);
+//         result((quint64) k, corpus[k], 6);
+    }
+}
+
+QTEST_APPLESS_MAIN(RFC4226TestVectorCompatTest)
+
+#include "rfc4226-hotp-compat.moc"
--- autotests/otp/sailotp-hotp-compat.cpp
+++ autotests/otp/sailotp-hotp-compat.cpp
@@ -45,6 +45,11 @@ void SailOTPHOTPCompatTest::test(const otp::Encoder& encoder)
     });
 
     qDebug() << "test:: key is:"<< secret;
+    if(secret.endsWith(QLatin1Char('=')))
+    {
+        QEXPECT_FAIL("", "SailOTP does something odd when padding is involved", Continue);
+    }
+
     QTEST(otp::token(secret, msg, m_key, m_algo, encoder), "sailotp");
 }
 
@@ -71,13 +76,38 @@ void SailOTPHOTPCompatTest::testHOTPDefault(void)
 void SailOTPHOTPCompatTest::testSteam_data(void)
 {
     setupDataColumns();
-    result("steam, counter = 0", otp::base32::recode(QLatin1String("hello, brave new world!")), 0, QLatin1String("todo")); // 955109
+    result("steam, steam_alphabet (1)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7ce2f, QLatin1String("N93YR"));
+    result("steam, steam_alphabet (2)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7ce35, QLatin1String("MT57J"));
+    result("steam, steam_alphabet (3)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7cea3, QLatin1String("WT25W"));
+    result("steam, steam_alphabet (4)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7cea5, QLatin1String("P7G7F"));
+    result("steam, steam_alphabet (5)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7cea8, QLatin1String("C4VM7"));
+    result("steam, steam_alphabet (6)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7cf43, QLatin1String("56M54"));
+    result("steam, steam_alphabet (7)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7cfd5, QLatin1String("H9J7W"));
+    result("steam, steam_alphabet (8)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7cfda, QLatin1String("2N8RH"));
+    result("steam, steam_alphabet (9)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7cfe1, QLatin1String("GGKCG"));
+    result("steam, steam_alphabet (10)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7df67, QLatin1String("CB2M4"));
+    result("steam, steam_alphabet (11)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7df6f, QLatin1String("GVBQ4"));
+    result("steam, steam_alphabet (12)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7df7f, QLatin1String("MXXD3"));
+    result("steam, steam_alphabet (13)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7df87, QLatin1String("KT248"));
+    result("steam, steam_alphabet (14)", QLatin1String("STEAMBCDFGHIJKLNOPQRUVWXYZ234567"), 0x0000000002e7dfb0, QLatin1String("39G97"));
 }
 
 void SailOTPHOTPCompatTest::testHOTPDefault_data(void)
 {
     setupDataColumns();
-    result("hotp, counter = 0", otp::base32::recode(QLatin1String("hello, brave new world!")), 1, QLatin1String("323049"));
+    result("hotp, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567' [0]", QLatin1String("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"), 0, QLatin1String("081962"));
+    result("hotp, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567' [1]", QLatin1String("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"), 1, QLatin1String("408553"));
+    result("hotp, 'ABCDEFGHIJKLMNOP' [0]", QLatin1String("ABCDEFGHIJKLMNOP"), 0, QLatin1String("827178"));
+    result("hotp, 'ABCDEFGHIJKLMNOP' [1]", QLatin1String("ABCDEFGHIJKLMNOP"), 1, QLatin1String("317963"));
+    result("hotp, 'YZ234567QQ======' [0]", QLatin1String("YZ234567QQ======"), 0, QLatin1String("103820"));
+    result("hotp, 'YZ234567QQ======' [1]", QLatin1String("YZ234567QQ======"), 1, QLatin1String("947745"));
+    result("hotp, 'ZZPLURALZ9ALPHA=' [0]", QLatin1String("ZZPLURALZ9ALPHA="), 0, QLatin1String("556415"));
+    result("hotp, 'ZZPLURALZ9ALPHA=' [1]", QLatin1String("ZZPLURALZ9ALPHA="), 1, QLatin1String("687812"));
+    result("hotp, 'OAKTREEFIBERS===' [0]", QLatin1String("OAKTREEFIBERS==="), 0, QLatin1String("008223"));
+    result("hotp, 'OAKTREEFIBERS===' [1]", QLatin1String("OAKTREEFIBERS==="), 1, QLatin1String("525482"));
+    result("hotp, 'QUETZALCOATL====' [0]", QLatin1String("QUETZALCOATL===="), 0, QLatin1String("839139"));
+    result("hotp, 'QUETZALCOATL====' [1]", QLatin1String("QUETZALCOATL===="), 1, QLatin1String("871480"));
+    result("hotp, counter = 0", otp::base32::recode(QLatin1String("hello, brave new world!")), 0, QLatin1String("009733"));// QLatin1String("323049"));
 }
 
 QTEST_APPLESS_MAIN(SailOTPHOTPCompatTest)
